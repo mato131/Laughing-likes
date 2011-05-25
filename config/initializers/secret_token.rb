bc0b438e736c4ec0c33b2f99590a76a009e7e212
Mohamed Loutfi <mato131@gmail.com>
2011-05-26 00:10:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laughinglikes::Application.config.secret_token = 'd65dfca30f8e0221a660532c993d9da6cb60461bc01b3e0e3cf0c49cccfc1c6279f9c10a4e646458f0dd7803fedfaa9019ede76701534fb7a60ae4717ded8b36'
